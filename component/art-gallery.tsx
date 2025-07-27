c092c3b03bf609ebae4a99c785f4bce9ece67281
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-07-27 17:38:33 +0500

    change the setting's

--- component/art-gallery.tsx
+++ component/art-gallery.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 
 export const ArtGallerySection = () => {
   return (
-    <div className="flex relative justify-center items-center p-20 gap-20 bg-[#F1F5F9]">
-      <Button className="absolute left-15 z-20 bg-white to-transparent flex items-center justify-between rounded-full">
+    <div className="flex flex-col lg:flex-row relative justify-center items-center py-12 px-4 gap-8 lg:p-20 lg:gap-20 bg-[#F1F5F9]">
+      <Button className="absolute left-[-50px] lg:left-15 lg:z-20 bg-white to-transparent lg:flex lg:items-center lg:justify-between rounded-full">
         <ChevronLeft className="text-blue-600" />
       </Button>
 
-      <Button className="absolute right-5 z-20 bg-white to-transparent flex items-center justify-between rounded-full">
+      <Button className="absolute right-[-50px] lg:right-5 lg:z-20 bg-white to-transparent lg:flex lg:items-center lg:justify-between rounded-full">
         <ChevronRight className="text-blue-600" />
       </Button>
 
-      <div className="gap-[10px] fiex-1">
+      <div className="gap-[10px] fiex-1 min-h-[258px]">
         <Image
           src="/pic/girl-video.png"
           alt="Girl Video"
@@ -29,7 +29,7 @@ export const ArtGallerySection = () => {
           </h2>
         </div>
         <div className="flex flex-col justify-center gap-4">
-          <p className="font-normal text-[24px] leading-[160%]">
+          <p className="font-normal text-[16px] lg:text-[24px] leading-[140%] lg:leading-[160%]">
             “Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis
             est velit in. Nibh in purus sit convallis phasellus ut. Neque
             suspendisse a habitasse commodo.”
