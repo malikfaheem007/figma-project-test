9e579d0f8e1f9f4157d63b005dd00205df40da7d
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-07-27 17:38:57 +0500

    change the setting's

--- component/carousel-2.tsx
+++ component/carousel-2.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export const CarouselSection2 = () => {
   return (
-    <div className="flex items-center bg-[#475569] p-20 gap-20">
+    <div className="flex flex-col lg:flex-row items-center bg-[#475569] py-12 gap-8 px-4 lg:p-20 lg:gap-20">
       <div className="gap-6 flex-3/8">
         <div className="gap-6">
-          <h2 className="font-extrabold text-[56px] text-white gap-2 leading-[110.000000000000%]">
+          <h2 className="font-bold text-[32px] lg:font-extrabold lg:text-[56px] text-white gap-2 leading-[110.000000000000%]">
             Be incredible
           </h2>
         </div>
         <div>
-          <p className="font-normal text-[18px] leading-[160%] text-white my-4">
+          <p className="font-normal text-[16px] lg:text-[18px] leading-[140%] lg:leading-[160%] text-white my-4">
             Nec ultricies eget placerat ultricies eleifend dignissim aliquet
             sapien. Senectus vestibulum, eget erat at et congue cursus pretium.
           </p>
@@ -22,8 +22,24 @@ export const CarouselSection2 = () => {
           </Button>
         </div>
       </div>
-      <div>
+      <div className="hidden lg:block">
         <Image src="/pic/album.png" alt="Album Pic" width={800} height={276} />
+      </div>
+      <div className="flex lg:hidden gap-6">
+        <Image
+          src="/pic/face.png"
+          alt="Face Pic"
+          width={300}
+          height={276}
+          className="lg:hidden"
+        />
+        <Image
+          src="/pic/side-background.png"
+          alt="Album Pic"
+          width={184}
+          height={276}
+          className="lg:hidden"
+        />
       </div>
     </div>
   );
