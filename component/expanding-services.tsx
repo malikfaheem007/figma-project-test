e8865ccb7783f1ee0c151a42804caa3589017e7e
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-07-27 17:39:48 +0500

    change the setting's

--- component/expanding-services.tsx
+++ component/expanding-services.tsx
@@ -3,59 +3,57 @@ import Image from "next/image";
 
 export const ExpandingServicesSection = () => {
   return (
-    <div className="flex flex-col p-20 gap-20 border-b-[1px] border-white">
+    <div className="flex flex-col gap-12 px-4 py-12 w-full lg:p-20 lg:gap-20">
       <div className="flex items-center justify-center gap-6">
-        <h2 className="font-bold text-2xl text-[#0F172A]">
+        <h2 className="font-bold text-[32px] lg:text-2xl text-[#0F172A]">
           Expanding Services
         </h2>
       </div>
-      <div className="flex justify-between">
-        <div className="gap-4">
+      <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
+        <div className="flex lg:w-full lg:justify-evenly">
           <Image
             src="/pic/nibh.png"
             alt="Nibh Image"
             width={173.33}
             height={102}
+            className="min-w-[104.33px] h-[124px]"
           />
-        </div>
-        <div>
           <Image
             src="/pic/cursus.png"
             alt="Cursus Image"
             width={173.33}
             height={102}
+            className="min-w-[104.33px] h-[124px]"
           />
-        </div>
-        <div>
           <Image
             src="/pic/iqsum.png"
             alt="Iqsum Image"
             width={173.33}
             height={102}
+            className="min-w-[104.33px] h-[124px]"
           />
         </div>
-        <div>
+        <div className="flex lg:w-full lg:justify-evenly">
           <Image
             src="/pic/quisque.png"
             alt="Quisque Image"
             width={173.33}
             height={102}
+            className="min-w-[88.33px] h-[124px]"
           />
-        </div>
-        <div>
           <Image
             src="/pic/arcu.png"
             alt="Arcu Image"
             width={173.33}
             height={102}
+            className="min-w-[88.33px] h-[124px]"
           />
-        </div>
-        <div>
           <Image
             src="/pic/cras.png"
             alt="Cras Image"
             width={173.33}
             height={102}
+            className="min-w-[88.33px] h-[124px]"
           />
         </div>
       </div>
