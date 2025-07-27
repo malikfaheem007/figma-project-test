8a7e68762d0c2a31f6a5d511be589bc86530bdb8
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-07-27 17:39:28 +0500

    change the setting's

--- component/carousel.tsx
+++ component/carousel.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 export const CarouselSection = () => {
   return (
-    <div className="flex flex-col relative border-b-[1px] border-[F1F5F9] py-20 gap-6 bg-[#F1F5F9]">
+    <div className="flex flex-col relative py-12 lg:py-20 gap-6 bg-[#F1F5F9]">
       <div>
         <Button className="absolute top-70 right-5 bg-gradient-to-b from-white to-transparent flex items-center justify-between rounded-full">
           <ChevronRight className="text-blue-600" />
         </Button>
       </div>
-      <div className="flex justify-between items-centergap-6 px-20">
-        <div className="gap-6">
-          <h2 className="text-2xl font-bold items-center flex">
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center lg:gap-6 px-4 lg:px-20">
+        <div className=" lg:gap-6 gap-2">
+          <h2 className="text-[32px] lg:text-2xl font-bold lg:items-center flex">
             Latest masterpieces
           </h2>
         </div>
-        <div className="flex rounded-[8px] items-center justify-center py-3 hover:underline">
+        <div className="flex rounded-[8px] lg:items-center lg:justify-center py-3 hover:underline ">
           <a
             href="/art-categories/paintings"
             className="text-blue-500 font-medium leading-[24px]"
@@ -28,43 +28,35 @@ export const CarouselSection = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-6 px-20">
-        <div className="flex items-center justify-center">
-          <Image
-            src="/pic/flower.png"
-            alt="Flower picture"
-            className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px]"
-            width={300}
-            height={300}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <Image
-            src="/pic/heart.png"
-            alt="Heart picture"
-            className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px]"
-            width={300}
-            height={300}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <Image
-            src="/pic/splash.png"
-            alt="Splash picture"
-            className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px]"
-            width={300}
-            height={300}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <Image
-            src="/pic/face.png"
-            alt="Face picture"
-            className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px]"
-            width={300}
-            height={300}
-          />
-        </div>
+      <div className="flex gap-6 px-4 lg:px-20 items-center">
+        <Image
+          src="/pic/flower.png"
+          alt="Flower picture"
+          className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px] min-w-[300px]"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/pic/heart.png"
+          alt="Heart picture"
+          className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px] min-w-[300px]"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/pic/splash.png"
+          className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px] min-w-[300px]"
+          alt="Splash picture"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/pic/face.png"
+          alt="Face picture"
+          className="object-cover border-[1px] border-[#E2E8F0] rounded-[10px] min-w-[300px]"
+          width={300}
+          height={300}
+        />
       </div>
     </div>
   );
