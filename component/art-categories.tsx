ce2acf7f3be6d3fed219e24120a7692f1353a572
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-07-27 17:37:48 +0500

    change the setting's

--- component/art-categories.tsx
+++ component/art-categories.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export const ArtCategoriesSection = () => {
   return (
-    <div className="bg-white p-20 gap-6 flex flex-col border-b-[1px] border-white">
-      <div className="flex justify-between items-center gap-6">
-        <div className="gap-6">
-          <h2 className="text-2xl font-bold items-center flex">
+    <div className="bg-white py-12 px-4 lg:p-20 gap-8 flex flex-col">
+      <div className="flex flex-col lg:flex-row lg:h-12 lg:justify-between gap-2 lg:gap-6">
+        <div className="flex lg:items-center ">
+          <h2 className="text-[32px] lg:text-2xl font-bold flex leading-[110.000000000000%]">
             Featured art categories
           </h2>
         </div>
-        <div className="flex h-12 rounded-[8px] items-center justify-center py-3 hover:underline">
-          <a href="/art-categories/paintings" className="text-blue-500 px-4">
+        <div className="flex lg:h-12 rounded-[8px] hover:underline items-center">
+          <a href="/art-categories/paintings" className="text-blue-500">
             View All Categories
           </a>
           <div className="text-center">
@@ -19,52 +19,43 @@ export const ArtCategoriesSection = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-6">
-        <div className="rounded-[5px]">
-          <Image
-            src="/pic/family.png"
-            alt="Family Art"
-            width={236.8}
-            height={354}
-            className="object-cover transform transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-        <div className="rounded-[5px]">
-          <Image
-            src="/pic/camel.png"
-            alt="Camel Art"
-            width={236.8}
-            height={354}
-            className="object-cover transform transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-        <div className="rounded-[5px]">
-          <Image
-            src="/pic/euforia.png"
-            alt="Euforia Art"
-            width={236.8}
-            height={354}
-            className="object-cover transform transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-        <div className="rounded-[5px]">
-          <Image
-            src="/pic/watch.png"
-            alt="Watch Art"
-            width={236.8}
-            height={354}
-            className="object-cover transform transition-transform duration-300 hover:scale-105"
-          />
-        </div>
-        <div className="rounded-[5px]">
-          <Image
-            src="/pic/angel.png"
-            alt="Angel Art"
-            width={236.8}
-            height={354}
-            className="object-cover transform transition-transform duration-300 hover:scale-105"
-          />
-        </div>
+      <div className="flex gap-6 w-auto">
+        <Image
+          src="/pic/family.png"
+          alt="Family Art"
+          width={236.8}
+          height={354}
+          className="object-cover transform transition-transform duration-300 hover:scale-105 min-w-[236.8px]"
+        />
+        <Image
+          src="/pic/camel.png"
+          alt="Camel Art"
+          width={236.8}
+          height={354}
+          className="object-cover transform transition-transform duration-300 hover:scale-105 min-w-[236.8px]"
+        />
+
+        <Image
+          src="/pic/euforia.png"
+          alt="Euforia Art"
+          width={236.8}
+          height={354}
+          className="object-cover transform transition-transform duration-300 hover:scale-105 min-w-[236.8px]"
+        />
+        <Image
+          src="/pic/watch.png"
+          alt="Watch Art"
+          width={236.8}
+          height={354}
+          className="object-cover transform transition-transform duration-300 hover:scale-105 min-w-[236.8px]"
+        />
+        <Image
+          src="/pic/angel.png"
+          alt="Angel Art"
+          width={236.8}
+          height={354}
+          className="object-cover transform transition-transform duration-300 hover:scale-105 min-w-[236.8px]"
+        />
       </div>
     </div>
   );
